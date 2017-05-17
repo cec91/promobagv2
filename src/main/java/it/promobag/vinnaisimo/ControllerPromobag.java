87c5dac0be31c56087be14566e14fa6f21a42609
cec91 <vincenzo.santucci1@gmail.com>
2017-05-17 11:36:39 +0200

    sixth commit

---
+++ src/main/java/it/promobag/vinnaisimo/ControllerPromobag.java
@@ -0,0 +1,27 @@
+package it.promobag.vinnaisimo;
+
+import it.promobag.vinnaisimo.Dao.UserDaoImpl;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by vsantucc on 17/05/2017.
+ */
+
+@RestController
+public class ControllerPromobag {
+
+    @RequestMapping(value="/promobag/hello")
+    public String sayHello(){
+        return "Hello from promobag";
+    }
+
+    @RequestMapping(value="/promobag/user")
+    public String user(){
+        UserDaoImpl userDao = new UserDaoImpl();
+        String namer = userDao.getUserByName("Vincenzo Santucci");
+        System.out.println("il nome preso dal database e: " + namer);
+        return namer;
+
+    }
+}
