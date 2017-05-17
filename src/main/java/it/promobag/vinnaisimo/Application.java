ab04e23f33787f882dbef798d82f892303a463e7
cec91 <vincenzo.santucci1@gmail.com>
2017-05-17 11:07:24 +0200

    Second commit

--- src/main/java/it/promobag/vinnaisimo/Application.java
+++ src/main/java/it/promobag/vinnaisimo/Application.java
@@ -3,17 +3,23 @@ package it.promobag.vinnaisimo;
 import it.promobag.vinnaisimo.Dao.UserDaoImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * Created by vsantucc on 17/05/2017.
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         UserDaoImpl userDao = new UserDaoImpl();
         userDao.insertUser();
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 }
